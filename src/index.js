8fb025ba34d80151496adeb5c6736d34c7762239
Luis Chavez <josephsiul15@gmail.com>
2020-06-05 19:21:29 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const getMaxNumber = (numbersArray) =>
+   numbersArray.reduce((accumulator, value) =>
+      value > accumulator ? value : accumulator
+   );
+
+const getMinNumber = (numbersArray) =>
+   numbersArray.reduce((accumulator, value) =>
+      value < accumulator ? value : accumulator
+   );
+
+const getAverage = (numbersArray) =>
+   numbersArray.reduce((accumulator, value, index) =>
+      index !== numbersArray.length - 1
+         ? accumulator + value
+         : (value + accumulator) / numbersArray.length
+   );
+
+module.exports = { getMaxNumber, getMinNumber, getAverage };
